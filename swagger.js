49d1e24745aa0711b9fd9a0a5d9f2b330293a30e
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-04-25 11:29:32 -0500

    Arerglo de swagger

---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const path = require("path");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Documentación de la API",
+      version: "1.0.0",
+      description: "Documentación  de la API de Simulación por Computador",
+    },
+  },
+  apis: [`${path.join(__dirname, "./routes/*.js")}`], // Rutas donde se encuentran los comentarios JSDoc
+};
+
+exports.swaggerSpecs = swaggerJsdoc(options);
