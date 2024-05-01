a498104b4229244dead612ac8a24ddaa52829b5c
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-05-01 17:00:28 -0500

    Arreglo de paginación

--- utils/requestHandler.js
+++ utils/requestHandler.js
@@ -1,8 +1,8 @@
-exports.handleRequest =  async (res, asyncCallback) => {
-    try {
-        const response = await asyncCallback();
-        res.status(response.status).json(response);
-      } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-      }
-}
+exports.handleRequest = async (res, asyncCallback) => {
+  try {
+    const response = await asyncCallback();
+    res.status(response.status).json(response);
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+};
