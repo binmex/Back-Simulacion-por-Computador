1640c1ae775da7522017e82c339a27d4078e5ae8
DianaDiazDS <93956532+DianaDiazDS@users.noreply.github.com>
2024-05-15 12:51:59 -0500

    grupo2

--- controllers/inscriptionsController.js
+++ controllers/inscriptionsController.js
@@ -89,6 +89,100 @@ const Topic = require("../models/topic-model");
 //     res.status(500).json({ success: false, error: error.message });
 //   }
 // };
+// exports.saveInscription = async (req, res) => {
+//   try {
+//     const { student, group, registrationDate, status } = req.body;
+//     let targetGroup = await Group.findById(group._id);
+//     if (!targetGroup) {
+//       return res.status(404).json({ success: false, error: "Grupo no encontrado" });
+//     }
+
+//     // Vrificar si el estudiante ya está inscrito en el grupo
+//     const existingInscription = await Inscription.findOne({ student: student, group: group._id });
+//     if (existingInscription) {
+//       return res.status(400).json({ success: false, error: "El estudiante ya está inscrito en este grupo" });
+//     }
+
+//     // el estudiante ya está inscrito en algun< otro grupo de la misma materia
+//     const topicGroups = await Group.find({ topic: targetGroup.topic }).populate('topic');
+
+//     for (const topicGroup of topicGroups) {
+//       const existingInscription = await Inscription.findOne({ student: student, group: topicGroup._id });
+//       if (existingInscription) {
+//         return res.status(400).json({ success: false, error: "El estudiante ya está inscrito en otro grupo de la misma materia" });
+//       }
+//     }
+
+//     // Verificar si hay cupos disponibles en el grupo     
+
+//     // Si no hay cupos buscar o crear un nuevo grupo
+//     if (targetGroup.quotas <= 0) {
+
+//       const alternativeGroups = await Group.find({
+//         _id: { $ne: group._id },
+//         name: targetGroup.name,
+//         quotas: { $gt: 0 }
+//       });
+
+//       const topic = await Topic.findById(targetGroup.topic);
+//       if (!topic) {
+//         return res.status(404).json({ success: false, error: "Materia no encontrada" });
+//       }
+//       // Si hay grupos alternativos disponibles
+//       if (alternativeGroups.length > 0) {
+//         targetGroup = alternativeGroups[0];
+//       } else {
+//        //para que genere nombre grupo1 grupo2.... pero de la misma materia
+//         const lastGroup = await Group.findOne({ topic: topic._id }).sort({ grupo: -1 });
+
+//         let newGroupName = "grupo1";
+//         if (lastGroup) {
+
+//           const lastGroupNumber = parseInt(lastGroup.grupo.replace("grupo", ""));
+//           newGroupName = `grupo${lastGroupNumber + 1}`;
+//         }
+
+
+
+//         // Crear un nuevo grupo 
+//         // const newGroup = new Group({
+//         //   grupo: newGroupName,
+//         //   name: targetGroup.name,
+//         //   topic: targetGroup.topic,
+//         //   quotas: topic.quotas, //  cuotas de la materia original
+//         // });
+//         const newGroup = new Group({
+//           grupo: newGroupName,
+//           name: originalGroup.name,
+//           topic: originalTopic._id,
+//           quotas: originalTopic.quotas,
+//         });
+        
+
+//         await newGroup.save();
+        
+
+//         targetGroup = newGroup;
+//       }
+//     }
+//     // inscribir al estudiante 
+//     const newInscription = new Inscription({
+//       student: student,
+//       group: targetGroup._id,
+//       registrationDate,
+//       status,
+//     });
+
+//     await newInscription.save();
+//     targetGroup.quotas--; // quitar cupos
+//     await targetGroup.save();
+
+//     return res.status(201).json({ success: true, data: newInscription });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ success: false, error: error.message });
+//   }
+// };
 exports.saveInscription = async (req, res) => {
   try {
     const { student, group, registrationDate, status } = req.body;
@@ -98,7 +192,7 @@ exports.saveInscription = async (req, res) => {
     }
 
     // Vrificar si el estudiante ya está inscrito en el grupo
-    const existingInscription = await Inscription.findOne({ student: student, group: group._id });
+    const existingInscription = await Inscription.findOne({ student: student._id, group: group._id });
     if (existingInscription) {
       return res.status(400).json({ success: false, error: "El estudiante ya está inscrito en este grupo" });
     }
@@ -107,7 +201,7 @@ exports.saveInscription = async (req, res) => {
     const topicGroups = await Group.find({ topic: targetGroup.topic }).populate('topic');
 
     for (const topicGroup of topicGroups) {
-      const existingInscription = await Inscription.findOne({ student: student, group: topicGroup._id });
+      const existingInscription = await Inscription.findOne({ student: student._id, group: topicGroup._id });
       if (existingInscription) {
         return res.status(400).json({ success: false, error: "El estudiante ya está inscrito en otro grupo de la misma materia" });
       }
@@ -145,29 +239,21 @@ exports.saveInscription = async (req, res) => {
 
 
         // Crear un nuevo grupo 
-        // const newGroup = new Group({
-        //   grupo: newGroupName,
-        //   name: targetGroup.name,
-        //   topic: targetGroup.topic,
-        //   quotas: topic.quotas, //  cuotas de la materia original
-        // });
         const newGroup = new Group({
           grupo: newGroupName,
-          name: originalGroup.name,
-          topic: originalTopic._id,
-          quotas: originalTopic.quotas,
+          name: targetGroup.name,
+          topic: targetGroup.topic,
+          quotas: topic.quotas, //  cuotas de la materia original
         });
-        
 
         await newGroup.save();
-        
 
         targetGroup = newGroup;
       }
     }
     // inscribir al estudiante 
     const newInscription = new Inscription({
-      student: student,
+      student: student._id,
       group: targetGroup._id,
       registrationDate,
       status,
@@ -183,7 +269,6 @@ exports.saveInscription = async (req, res) => {
     res.status(500).json({ success: false, error: error.message });
   }
 };
-
 
 exports.findAllInscription = async (req, res) => {
   try {
