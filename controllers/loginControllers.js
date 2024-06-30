a4319f5706f9ffc68754e50c27649d3a5ae37bc9
Luis Enrique Jr. Pinto Fuentes <91229941+LuisPinto9@users.noreply.github.com>
2024-06-30 12:09:58 -0500

    Arreglode validación

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -88,7 +88,7 @@ exports.validate = async (req, res) => {
       const token = jwt.createToken(user);
       res.status(200).json({
         state: true,
-        data: { message: "Usuario encontrado", image: user.image, username: user.username},
+        data: { message: "Usuario encontrado", image: user.image, username: user.username, role: user.role},
         token,
       });
     } catch (err) {
